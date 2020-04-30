87ea3e0c819ee9a839638647301db45feb781e6b
EliJaghab <elijaghab@gmail.com>
2020-04-30 14:33:56 -0400

    Cost Screen Functioning Within Nav

--- App.js
+++ App.js
@@ -1,236 +1,82 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Lists, TripSum, CostSum } from './Screens';
+import Icon from 'react-native-vector-icons/Ionicons';
+import List from './List.js';
+import Trip from './Trip.js';
+import Cost from './Cost.js';
 
-// Used this code - has the packages that you need to install 
-// https://aboutreact.com/collapsible-accordion-expandable-view/
-/*Example of Collapsible - Accordion - Expandable View in React Native*/
->>>>>>> 8637cb4071b76f1dbaef29cdc83d9aad03913681
-import React, { Component } from 'react';
-import {Switch,ScrollView,StyleSheet,Text,View,TouchableOpacity,} from 'react-native';
-import * as Animatable from 'react-native-animatable';
-import Collapsible from 'react-native-collapsible';
-import Accordion from 'react-native-collapsible/Accordion';
+const Tabs = createBottomTabNavigator();
+const ListStack = createStackNavigator();
+const TripStack = createStackNavigator();
+const CostStack = createStackNavigator();
 
+const ListStackScreen = () => (
+  <ListStack.Navigator>
+    <ListStack.Screen name = "All Lists" component = {List}/>
+  </ListStack.Navigator>
+);
 
-const CONTENT = [
-  {
-    title: 'Past Lists',
-    content:
-      '',
-  },
-  {
-    title: 'New List',
-    content:
-      '',
-  },
-  {
-    title: 'Receipts',
-    content:
-      '',
-  },
-  {
-    title: 'Trip Summaries',
-    content:
-      '',
-  },
-  {
-    title: 'Cost Summaries',
-    content:
-      '',
-  },
-];
+const TripStackScreen = () => (
+  <TripStack.Navigator>
+    <TripStack.Screen name = "Trip Summaries" component = {Trip}/>
+  </TripStack.Navigator>
+);
 
-//To make the selector (Something like tabs)
-const SELECTORS = [
-  { title: 'Past', value: 0 },
-  { title: 'New', value: 1 },
-  { title: 'Receipts', value: 2 },
-  { title: 'Trips', value: 3 },
-  { title: 'Costs', value: 4 },
-  { title: 'Close All' },
-];
+const CostStackScreen = () => (
+  <CostStack.Navigator>
+    <CostStack.Screen name = "Cost Summaries" component = {Cost}/>
+  </CostStack.Navigator>
+);
 
-export default class App extends Component {
-  state = {
-    //default active selector
-    activeSections: [],
-    //collapsed condition for the single collapsible
-    collapsed: true,
-    //multipleSelect is for the Multiple Expand allowed
-    //true: You can expand multiple at a time
-    //false: One can be expand at a time and other will be closed automatically
-    multipleSelect: false,
-  };
+export default () => (
+  <NavigationContainer>
+    <Tabs.Navigator initialRouteName="Lists" tabBarOptions={{activeColor: '#3C505C', inactiveColor: '#4C97C2', barStyle: { backgroundColor: '#96CED8' }, shifting: true,}}>
 
-  toggleExpanded = () => {
-    //Toggling the state of single Collapsible
-    this.setState({ collapsed: !this.state.collapsed });
-  };
+      <Tabs.Screen 
+          name = "Lists" 
+          component = {ListStackScreen} 
+          options={{
+            tabBarLabel: 'Lists',
+            tabBarIcon: ({ tintColor }) => (
+              <Icon style={[{color: tintColor}]} size={25} name={'ios-list'}/>
+            ),
+            activeColor: '#3C505C',
+            inactiveColor: '#4C97C2',
+            barStyle: { backgroundColor: '#96CED8' },
+          }}
+      />
 
-  setSections = sections => {
-    //setting up a active section state
-    this.setState({
-      activeSections: sections.includes(undefined) ? [] : sections,
-    });
-  };
+      <Tabs.Screen 
+          name = "Trip Summaries" 
+          component = {TripStackScreen}
+          options={{
+          tabBarLabel: 'Trip Summaries',
+            tabBarIcon: ({ tintColor }) => (
+              <Icon style={[{color: tintColor}]} size={25} name={'ios-basket'}/>
+            ),
+          activeColor: '#3D7C39',
+          inactiveColor: '#48AB34',
+          barStyle: { backgroundColor: '#7FD979' },
+          }}
+      />
 
-  renderHeader = (section, _, isActive) => {
-    //Accordion Header view
-    return (
-      <Animatable.View
-        duration={400}
-        style={[styles.header, isActive ? styles.active : styles.inactive]}
-        transition="backgroundColor">
-        <Text style={styles.headerText}>{section.title}</Text>
-      </Animatable.View>
-    );
-  };
+      <Tabs.Screen 
+          name = "Cost Summaries" 
+          component = {CostStackScreen}
+          options={{
+            tabBarLabel: 'Cost Summaries',
+              tabBarIcon: ({ tintColor }) => (
+                <Icon style={[{color: tintColor}]} size={25} name={'ios-trending-up'}/>
+              ),
+            activeColor: '#AE2727',
+            inactiveColor: '#CC5D5D',
+            barStyle: { backgroundColor: '#E57F7F' },
+          }}
+      />
 
-  renderContent(section, _, isActive) {
-    //Accordion Content view
-    return (
-      <Animatable.View
-        duration={400}
-        style={[styles.content, isActive ? styles.active : styles.inactive]}
-        transition="backgroundColor">
-        <Animatable.Text
-          animation={isActive ? 'bounceIn' : undefined}
-          style={{ textAlign: 'center' }}>
-          {section.content}
-        </Animatable.Text>
-      </Animatable.View>
-    );
-  }
-
-  render() {
-    const { multipleSelect, activeSections } = this.state;
-    return (
-      <View style={styles.container}>
-        <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
-          <Text style={styles.title}>SmartShop</Text>
-          <View style={{ backgroundColor: '#000', height: 1, marginTop: -10 }} />
-
-          {/*Code for Accordion/Expandable List starts here*/}
-          <Accordion
-            activeSections={activeSections}
-            //for any default active section
-            sections={CONTENT}
-            //title and content of accordion
-            touchableComponent={TouchableOpacity}
-            //which type of touchable component you want
-            //It can be the following Touchables
-            //TouchableHighlight, TouchableNativeFeedback
-            //TouchableOpacity , TouchableWithoutFeedback
-            expandMultiple={multipleSelect}
-            //Do you want to expand mutiple at a time or single at a time
-            renderHeader={this.renderHeader}
-            //Header Component(View) to render
-            renderContent={this.renderContent}
-            //Content Component(View) to render
-            duration={400}
-            //Duration for Collapse and expand
-            onChange={this.setSections}
-            //setting the state of active sections
-          />
-          {/*Code for Accordion/Expandable List ends here*/}
-        </ScrollView>
-
-                  {/*Code for Selector starts here*/}
-                  <View style={styles.selectors}>
-            {SELECTORS.map(selector => (
-              <TouchableOpacity
-                key={selector.title}
-                onPress={() => this.setSections([selector.value])}
-                //on Press of any selector sending the selector value to
-                // setSections function which will expand the Accordion accordingly
-              >
-                <View style={styles.selector}>
-                  <Text
-                    style={
-                      activeSections.includes(selector.value) &&
-                      styles.activeSelector
-                    }>
-                    {selector.title}
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            ))}
-          </View>
-          {/*Code for Selector ends here*/}
-
-        {/*Code for Toggle Button*/}
-        <View style={styles.multipleToggle}>
-            <Text style={styles.multipleToggle__title}>
-              Multiple Expand Allowed?
-            </Text>
-            <Switch
-              value={multipleSelect}
-              onValueChange={multipleSelect =>
-                this.setState({ multipleSelect })
-              }
-            />
-          </View>
-
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    paddingTop: 30,
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: 25,
-    fontWeight: '300',
-    marginBottom: 20,
-  },
-  header: {
-    backgroundColor: '#F5FCFF',
-    padding: 10,
-  },
-  headerText: {
-    textAlign: 'center',
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  content: {
-    padding: 20,
-    backgroundColor: '#fff',
-  },
-  active: {
-    backgroundColor: 'rgba(255,255,255,1)',
-  },
-  inactive: {
-    backgroundColor: 'rgba(245,252,255,1)',
-  },
-  selectors: {
-    marginBottom: 10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  selector: {
-    backgroundColor: '#F5FCFF',
-    padding: 10,
-  },
-  activeSelector: {
-    fontWeight: 'bold',
-  },
-  selectTitle: {
-    fontSize: 14,
-    fontWeight: '500',
-    padding: 10,
-    textAlign: 'center',
-  },
-  multipleToggle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginVertical: 30,
-    alignItems: 'center',
-  },
-  multipleToggle__title: {
-    fontSize: 16,
-    marginRight: 8,
-  },
-});
+    </Tabs.Navigator>
+  </NavigationContainer>
+);
